c6716773da0df39e5707652fc623b845bb6a1362
abhinav101010 <71586575+abhinav101010@users.noreply.github.com>
2026-07-30 18:31:28 +0530

    Revised Loops and functions

---
+++ Loops.java
@@ -0,0 +1,23 @@
+public class Loops {
+    public static void main(String[] args) {
+        // Loops
+        // for loop
+        // while loop
+        // do-while loop
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        int j = 0;
+        while (j < 10) {
+            System.out.println(j);
+            j++;
+        }
+
+        int k = 0;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k < 10);        
+    }
+}
